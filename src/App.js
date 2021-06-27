7ccba1ec05af0f6d323eaa01c95db65678f28fe0
RishikeshPolasa <rishipolasa@gmail.com>
2021-06-27 11:48:46 +0530

    Add header component

--- src/App.js
+++ src/App.js
@@ -1,7 +1,13 @@
 import React from "react";
+import Header from "./Header";
 
 function App() {
-  return <div className="app">Gbook</div>;
+  return (
+    <div className="app">
+      {/* <h1>Gbook</h1> */}
+      <Header />
+    </div>
+  );
 }
 
 export default App;
---
+++ src/firebase.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyAoTBF31G3RawjAK7JMO2Edn9dCkatJAqo",
+  authDomain: "gbook-366c4.firebaseapp.com",
+  projectId: "gbook-366c4",
+  storageBucket: "gbook-366c4.appspot.com",
+  messagingSenderId: "845806840084",
+  appId: "1:845806840084:web:0fbe8f40cf6574780fba01",
+};
